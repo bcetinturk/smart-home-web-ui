48ad3e0aecce6a5e00213459472e7d79870aa69f
Baris Cetinturk <baris.cetinturk.ytu@gmail.com>
2019-12-16 10:30:17 +0300

    user authentication using passport.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+require("dotenv").config()
 const bodyParser = require("body-parser")
-const mongoose = require("mongoose")
+const cookieParser = require("cookie-parser")
 const express = require("express")
+const mongoose = require("mongoose")
+const LocalStrategy = require('passport-local').Strategy;
+const passport = require("passport")
 const path = require("path")
+const session = require("express-session")
 const User = require("./models/user.model")
 const Usage = require("./models/usage.model")
 const userRouter = require("./routes/user")
@@ -13,10 +18,34 @@ mongoose.connect(mongoURI, {
 }).then(()=> console.log("connection to mongodb was successful"))
 .catch(err => console.log("error occured:", err))
 
+passport.use(new LocalStrategy(
+    function(username, password, done){
+        User.findOne({username}, function(err, user){
+            if(err) { return done(err)}
+            if(!user) { return done(null, false)}
+            if(!user.comparePassword(password)){return done(null, false)}
+            return done(null, user)
+        })
+    }))
+passport.serializeUser(function(user, done){
+    done(null, user)
+})
+passport.deserializeUser(function(user,done){
+    done(null, user)
+})
+
 const app = express()
 app.use(express.static("public"))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    saveUninitialized: true,
+    resave: true
+}))
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(userRouter)
 
 app.set("views", path.join(__dirname, "/templates/views"))
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,21 @@
 const express = require("express")
+const passport = require("passport")
 const router = express.Router()
 
 router.get("/login", (req, res)=>{
     res.render("login")
 })
 
-router.post("/login", (req, res)=>{
-    console.log(req.body)
+router.post("/login", passport.authenticate("local", {failureRedirect: "/login"}), 
+    (req, res)=>{
+        console.log(req.body)
+    res.redirect(`/user/${req.user._id}`)
+})
+
+router.get("/user/:id", (req, res)=>{
+    if(req.isAuthenticated()){
+        return res.render("index")
+    }
 })
 
 module.exports = router
