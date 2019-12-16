67ab9eb4ce18a689ef71c8defc13d47da7cc4f1a
Baris Cetinturk <baris.cetinturk.ytu@gmail.com>
2019-12-16 20:29:18 +0300

    buttons get their icons from database now

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const passport = require("passport")
 const router = express.Router()
+const User = require("../models/user.model")
+const mongoose = require("mongoose")
 
 router.get("/login", (req, res)=>{
     res.render("login")
@@ -11,8 +13,14 @@ router.post("/login", passport.authenticate("local", {failureRedirect: "/login"}
     res.redirect(`/user/${req.user._id}`)
 })
 
-router.get("/user/:id", (req, res)=>{
-    res.render("index")
+router.get("/user/:id", async (req, res)=>{
+    const user = await User.aggregate([
+        {$match: {_id: mongoose.Types.ObjectId(req.params.id)}},
+        {$lookup: {from: "devices", localField: "houseware.device", foreignField: "_id", as: "devices"}}
+    ])
+    console.log(user[0])
+
+    res.render("index", {devices: user[0].devices})
 })
 
 module.exports = router
